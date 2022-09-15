30a2a8779ea20c76262b77f67b6dc325d4983177
dylangriess <dylangriess@gmail.com>
2022-09-15 11:23:27 -0600

    transferred home route into group-routes.js

--- controllers/api/group-routes.js
+++ controllers/api/group-routes.js
@@ -1,4 +1,29 @@
 const router = require("express").Router();
+const { User, Group } = require("../../models");
+
+// GET group data on homepage for user
+router.get("/", async (req, res) => {
+  try {
+    const groupData = await Group.findAll({
+      include: [
+        {
+          model: Group,
+          attributes: ["name"],
+        },
+      ],
+    });
+
+    const allGroups = groupData.map((groups) => groups.get({ plain: true }));
+
+    res.render("homepage", {
+      allGroups,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 // Logout Group
 router.post("/logout", (req, res) => {
