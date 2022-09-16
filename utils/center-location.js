1643abc7c025e20c6d31116f31bd834fddd935a0
Hannah Alverson <alverson98@yahoo.com>
2022-09-16 15:50:11 -0600

    added calculations to get the average lat & long

--- utils/center-location.js
+++ utils/center-location.js
@@ -13,6 +13,19 @@ const getCenterLocation = async () => {
     //all of the user's longitude
     const allLong = { userLong: parseInt(json.longitude) };
 
+    //creating array of all user lats & longs
+    const allLatArray = Object.values(allLat);
+    const allLongArray = Object.values(allLong);
+
+    //sum of lat & long
+    const sumLat = allLatArray.reduce((acc, val) => acc + val, 0);
+    const sumLong = allLongArray.reduce((acc, val) => acc + val, 0);
+    
+    //average of lat & long
+    const averageLat = sumLat / allLatArray.length;
+    const averageLong = sumLong / allLongArray.length;
+
+    
   } catch (err) {
     console.log(err);
   }
