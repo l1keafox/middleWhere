ddf0315af2c38289c0fb8e0e55c96088ceb32a5a
dylangriess <dylangriess@gmail.com>
2022-09-16 14:44:47 -0600

    fixed map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,23 +1,27 @@
 // Initialize and add the map
 async function initMap() {
-    // The location of Uluru
-    const response = await fetch("/api/groups/1",{
-      methd:'GET',
-    });
-    let json = await response.json();
+  // The location of Uluru
+  const response = await fetch("/api/groups/1", {
+    method: "GET",
+  });
+  let json = await response.json();
 
-    const centerOfMap = { lat: parseFloat( json.latitude ), lng: parseFloat( json.longitude ) };
-    // The map, centered at Uluru
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 8,
-      center: centerOfMap,
-    });
-    
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-      position: centerOfMap,
-      map: map,
-    });
-  }
-  
-  window.initMap = initMap; 
+  const centerOfMap = {
+    lat: parseFloat(json.latitude),
+    lng: parseFloat(json.longitude),
+  };
+  console.log(centerOfMap);
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: centerOfMap,
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: centerOfMap,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
