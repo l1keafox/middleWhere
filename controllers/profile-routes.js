92a3083546ee4c431fd9da32cbcb2482cef37bd2
ZacharyFeltman <zachary.feltman@gmail.com>
2022-09-19 12:03:22 -0600

    display user name on profile page

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,11 +1,30 @@
 const router = require("express").Router();
-//const User = require("../../models/User.js");
+const User = require("../models/User");
+const Group = require("../models/Group");
 
 router.get("/", async (req, res) => {
-    try {
+  try {
+      let allUserGroups;
+      let noGroup;
+      if (req.session.user.groupId) {
+        let allCurUserNGroup = await Group.findAll ({
+          where: {
+            id: req.session.user.groupId,
+          },
+        });
+        console.log(allCurUserNGroup)
+        console.log(noGroup)
+        allUserGroups = allCurUserNGroup.map((group) =>
+          group.get({plain: true})
+          );
+          noGroup = false;
+      };
+
       res.render("profile", {
         user: req.session.user, // pasing user data for handlebars profile
         loggedIn: req.session.loggedIn,
+        allUserGroups,
+        noGroup,
       });
     } catch (err) {
       console.log(err);
@@ -14,3 +33,4 @@ router.get("/", async (req, res) => {
   });
   
 module.exports = router;
+
