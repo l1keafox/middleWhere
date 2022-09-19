4516a7a1d5c3ce7025e39185782e46580115e6fd
ZacharyFeltman <zachary.feltman@gmail.com>
2022-09-19 12:43:25 -0600

    user names presented

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -4,33 +4,16 @@ const Group = require("../models/Group");
 
 router.get("/", async (req, res) => {
   try {
-      let allUserGroups;
-      let noGroup;
-      if (req.session.user.groupId) {
-        let allCurUserNGroup = await Group.findAll ({
-          where: {
-            id: req.session.user.groupId,
-          },
-        });
-        console.log(allCurUserNGroup)
-        console.log(noGroup)
-        allUserGroups = allCurUserNGroup.map((group) =>
-          group.get({plain: true})
-          );
-          noGroup = false;
-      };
+    res.render("profile", {
+      user: req.session.user, // pasing user data for handlebars profile
+      loggedIn: req.session.loggedIn,
+      allUserGroups,
+      noGroup,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-      res.render("profile", {
-        user: req.session.user, // pasing user data for handlebars profile
-        loggedIn: req.session.loggedIn,
-        allUserGroups,
-        noGroup,
-      });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
-  
 module.exports = router;
-
