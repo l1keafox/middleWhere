c56e8fd3b62ffb3417283a5a6e9e94d8af00c9a8
Hannah Alverson <alverson98@yahoo.com>
2022-09-19 00:00:39 -0600

    added properties to render on map page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,51 +2,74 @@
 const router = require("express").Router();
 const { Group, User } = require("../models");
 router.get("/", async (req, res) => {
-    try {
-      // This is where we determeine what page to render for the user.
-      // If there is an session we will render map 
-      if(req.session && req.session.loggedIn){
-        // Here we should make an api/group/id# request to grab info for the map.
-        //
+  try {
+    // This is where we determeine what page to render for the user.
+    // If there is an session we will render map
+    if (req.session && req.session.loggedIn) {
+      // Here we should make an api/group/id# request to grab info for the map.
+      //
 
-
-        let currentUserGroup;
-        if(req.session.user.groupId){
-          currentUserGroup = await Group.findOne({
-            where: {
-              id: req.session.user.groupId,
-            },
-          });
-          }
-
-        let allMems;
-        let allGroups;
-        if( req.session.user.groupId) {
-          allGroups = await User.findAll({
-            where:{
-              groupId: req.session.user.groupId
-            },
-          });
-          allMems = allGroups.map((dish) => dish.get({ plain: true }));
-          }
-
-        res.render("map", {
-          members:allMems? allMems: null,
-          groupName:currentUserGroup? currentUserGroup.dataValues.name : null,
-          userInfo:req.session.user,
-          loggedIn: req.session.loggedIn,
+      let currentUserGroup;
+      if (req.session.user.groupId) {
+        currentUserGroup = await Group.findOne({
+          where: {
+            id: req.session.user.groupId,
+          },
         });
-      } else {
-        // If there is no session we will render Login.
-        res.render("login");
-        }
+      }
 
-      } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
+      //getting all user groups
+      let allUserGroups;
+      let noGroup;
+      if (req.session.user.groupId) {
+        currentUserGroups = await Group.findAll({
+          where: {
+            id: req.session.user.groupId,
+          },
+        });
+        allUserGroups = currentUserGroups.map((group) =>
+          group.get({ plain: true })
+        );
+        noGroup = false;
+      }
+
+      let allMems;
+      let allGroups;
+      if (req.session.user.groupId) {
+        allGroups = await User.findAll({
+          where: {
+            groupId: req.session.user.groupId,
+          },
+        });
+        allMems = allGroups.map((dish) => dish.get({ plain: true }));
+      }
+
+      //message stating the user has no groups
+      let noGroupMessage;
+      if (req.session.user.groupId === null) {
+        noGroupMessage = "You currently have no groups.";
+        noGroup = true;
+      }
+
+      res.render("map", {
+        members: allMems ? allMems : null,
+        groupName: currentUserGroup ? currentUserGroup.dataValues.name : null,
+        allUserGroups,
+        noGroupMessage,
+        noGroup,
+        userInfo: req.session.user,
+        loggedIn: req.session.loggedIn,
+      });
+    } else {
+      // If there is no session we will render Login.
+      res.render("login");
     }
-  });
-  
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 //use /utils/auth.js (getAuth) for any page that needs user to be logged in to access
 
 module.exports = router;
