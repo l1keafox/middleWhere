e49cc735f1f0f8dd0bb46ea51ac4f439f4f86348
Hannah Alverson <alverson98@yahoo.com>
2022-09-19 14:38:52 -0600

    added db query and properties to render on page

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -4,8 +4,27 @@ const Group = require("../models/Group");
 
 router.get("/", async (req, res) => {
   try {
+    let userGroup;
+    let noGroup;
+    if (req.session.user.groupId) {
+      let groupData = await Group.findAll({
+        where: {
+          id: req.session.user.groupId,
+        },
+        attributes: ["name"],
+      });
+      const userGroup = groupData.map((group) => group.name);
+      noGroup = false;
+    }
+
+    if (req.session.user.groupId === null) {
+      noGroup = true;
+    }
+
     res.render("profile", {
       user: req.session.user, // pasing user data for handlebars profile
+      userGroup,
+      noGroup,
       loggedIn: req.session.loggedIn,
     });
   } catch (err) {
